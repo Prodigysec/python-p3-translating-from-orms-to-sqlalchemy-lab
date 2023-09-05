92c08a7b182fcd038d99f99165908dce101a7ec9
Ian Raburu <williamraburu@gmail.com>
2023-09-05 10:47:12 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,42 @@
 from models import Dog
+from sqlalchemy import (Column, String, Integer)
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
+    __tablename__ = 'dogs'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog).all()
+    return dogs
 
 def find_by_name(session, name):
-    pass
+    dogs = session.query(Dog).filter_by(name=name).first()
+    return dogs
 
 def find_by_id(session, id):
-    pass
+    dogs = session.query(Dog).filter_by(id=id).first()
+    return dogs
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dogs = session.query(Dog).filter_by(name=name, breed=breed).first()
+    return dogs
 
 def update_breed(session, dog, breed):
-    pass
+    dogs = session.query(Dog).filter_by(id=dog.id).update({'breed': breed})
+    return dogs
+
+# if __name__ == '__main__':
+#     engine = create_engine('sqlite:///:memory:')
+#     Base.metadata.create_all(engine)
+
+#     # use our engine to configure a 'Session' class
+#     Session = sessionmaker(bind=engine)
+#     # use 'Session' class to create 'session' object
+#     session = Session()
